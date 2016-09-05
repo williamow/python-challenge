d22280b7495abce6a4b06fb57a6c55b60f5411b3
williamow <wow@dcn.org>
2016-09-04 21:17:33 -0700

    comments, attempt to return all instances
    added comments and trying to make return all instances, instead of just
    one incidence of the pattern at a time

--- level 3.py
+++ level 3.py
@@ -2,18 +2,22 @@ print("Let's solve level 3!")
 d = open("level 3.txt", "r").read()
 d = d.replace("\n\r", "")
 s = ""
-m = d.lower()
+m = d.lower() #all lowercase version
 for x in range(0, len(d)):
-    if(m[x] == d[x]):
-        s += "l"
+    if(m[x] == d[x]): #if they are equal, then original was lowercase
+        s += "l" #add l or U to create string of uppercase and lowercase
     else:
         s += "U"
 
-c = s.count("lUUUlUUUl")
+#returns number of occurances of pattern within string
+c = s.count("lUUUlUUUl") 
 print(c)
 lc = 10
-a = s.index("lUUUlUUUl")
-lc += a
-print(a)
-print(d[a+4])
+
+#returns index of first occurance of the propper uppercase-lowercase pattern
+for b in range (0, c):
+    a = s.index("lUUUlUUUl") 
+    print(a)
+    print(d[a+4])
+    s = s[(a+3):] #cuts a+3 characters from the front of the string
     
