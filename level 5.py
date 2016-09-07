59ca09c32685215e3388ddca54d24a427522ecb2
Megan Yamoah <yamoah.megan@gmail.com>
2016-09-06 21:05:56 -0400

    Text Doc

--- level 5.py
+++ level 5.py
@@ -1,7 +1,7 @@
 print("Level 5 is terrible!")
 import requests, pickle
 
-page = requests.get("http://www.pythonchallenge.com/pc/def/banner.p")
-stuf = page.text
+# page = requests.get("http://www.pythonchallenge.com/pc/def/banner.p")
+stuf = open("level 5.txt", "r").read()
 obj = pickle.load(stuf)
 print(obj)
