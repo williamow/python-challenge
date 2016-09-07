5d76e2800b11f28f1ba2ee9d9990f48bd9e3a3cb
Megan Yamoah <yamoah.megan@gmail.com>
2016-09-06 22:03:36 -0400

    This is hell.

--- level 5.py
+++ level 5.py
@@ -2,6 +2,6 @@ print("Level 5 is terrible!")
 import requests, pickle
 
 # page = requests.get("http://www.pythonchallenge.com/pc/def/banner.p")
-stuf = open("level 5.txt", "r").read()
+stuf = open("level 5.txt", "r+b")
 obj = pickle.load(stuf)
 print(obj)
